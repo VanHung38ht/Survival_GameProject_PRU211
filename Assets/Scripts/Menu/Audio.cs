38e8b82ac60824f08d2a28aea2ace3aff9ed6d78
unknown <donghiem114@gmail.com>
2023-03-05 17:53:11 +0700

    menu update

---
+++ Assets/Scripts/Menu/Audio.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    public static AudioClip fire;
+    public static AudioSource audioSource;
+
+    private void Start()
+    {
+        fire = Resources.Load<AudioClip>("fire");
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public static void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+            case "fire":
+                audioSource.PlayOneShot(fire);
+                break;
+        }
+    }
+}
