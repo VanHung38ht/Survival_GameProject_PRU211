1b1e9529726fd115c2f819f1deba0c8020d3e8ee
ngangiang2909 <ngangiang2909@gmail.com>
2023-03-07 22:16:30 +0700

    khai báo enemy

---
+++ Assets/Scripts/BaseData/DataManager/EnemyDataManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BaseData
+{
+    [CreateAssetMenu(fileName = "EnemyData", menuName = "DataManager/EnemyDataManager", order = 1)]
+    public class EnemyDataManager : ScriptableObject
+    {
+        public string className;
+        public string classDescription;
+
+        public float HP;
+        public float percentRegenHP = 0;
+        public float armor;
+
+        public float percentDamage = 0;
+        public float meleeDamage;
+        public float rangeDamage;
+        public float elementDamage;
+        public float percentLifeSteel = 0;
+        public float attackRange = 0;
+        public float range = 0;
+
+
+        public float percentAttackSpeed = 0;
+        public float Speed = 0;
+
+        public float xpDrop = 0;
+        public float percentExpGains = 0;
+        public float percentCoinGains = 0;
+    }
+}
