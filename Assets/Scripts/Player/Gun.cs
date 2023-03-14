2dc6127e62b7180b5a3e8e538c945a6f955519cb
unknown <donghiem114@gmail.com>
2023-03-15 00:21:39 +0700

    update gun

--- Assets/Scripts/Player/Gun.cs
+++ Assets/Scripts/Player/Gun.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-
+    Gun[] guns;
+    public int gunLength;
+    private Vector2 Direction;
+    public float Force;
     [SerializeField]
     private Transform bulletPoint;
 
@@ -24,5 +27,15 @@ public class Gun : MonoBehaviour
 
         GameObject BulletIns = Instantiate(Bullet, bulletPoint.position, Quaternion.identity);
         BulletIns.GetComponent<Rigidbody2D>().AddForce(Direction * Force);
+        guns[0].Shoot(Direction, Force);
+        Audio.PlaySound("fire");
+    }
+
+    private void ShootBulletHell()
+    {
+        for (int i = 0; i < gunLength; i++)
+        {
+            guns[i].Shoot(Direction, Force);
+        }
     }
 }
