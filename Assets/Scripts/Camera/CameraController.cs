e4648991a87404b8c01d33399a6ce087a670dadc
nguyenvanhunghe153434 <99272717+nguyenvanhunghe153434@users.noreply.github.com>
2023-03-20 22:53:15 +0700

    fix camera

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float FollowSpeed = 2f;
-    // public float yOffset = 1f;
-    public Transform target;
+     public float FollowSpeed = 2f;
+   //  public float yOffset = 1f;
+     public Transform target;
+
+   
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +19,11 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPoss = new Vector3(target.position.x, target.position.y /*+ yOffset*/, -10f);
-        transform.position = Vector3.Slerp(transform.position, newPoss, FollowSpeed*Time.deltaTime);
+        Vector3 newPoss = target.position;
+        newPoss.z = -10f;
+        transform.position = newPoss;
+        /*
+          Vector3 newPoss = new Vector3(target.position.x, target.position.y *//*+ yOffset*//*, -10f);
+          transform.position = Vector3.Slerp(transform.position, newPoss, FollowSpeed * Time.deltaTime);*/
     }
 }
